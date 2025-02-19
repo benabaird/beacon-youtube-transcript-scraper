9a651ea7e886ee51f5d27fe362040536e1c457b2
Benjamin Baird <benb@oho.com>
2025-02-19 16:23:21 -0600

    Install Symfony.

---
+++ src/Kernel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Beacon;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+}
